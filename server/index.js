3527a6165b7ad5c53038916a13b7161bf4f61c7e
Sindy Fuentes <sindy.fuentes@globant.com>
2020-10-01 15:36:25 -0500

    endpoint create product

--- server/index.js
+++ server/index.js
@@ -4,11 +4,25 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-const { getProduct } = require("../services/productService");
+const {
+  getProduct,
+  createProduct,
+  validateProductData,
+} = require("../services/productService");
 
 server.listen(3001, (req, res) => console.log("listening port 3001"));
 
 server.get("/api/v1/products/", getProduct, (req, res) => {
-  const { product } = req;
-  res.status(200).json(product);
+  const { products } = req;
+  res.status(200).json(products);
 });
+
+server.post(
+  "/api/v1/products/",
+  validateProductData,
+  createProduct,
+  (req, res) => {
+    const { product } = req;
+    res.status(200).json(product);
+  }
+);
--- services/productService.js
+++ services/productService.js
@@ -3,6 +3,20 @@ const axios = require("axios");
 const getProduct = async (req, res, next) => {
   try {
     const { data } = await axios.get("http://localhost:8080/api/v1/products");
+    req.products = data;
+    next();
+  } catch (error) {
+    res.status(500).send({
+      message: error,
+    });
+  }
+};
+
+const createProduct = async (req, res, next) => {
+  try {
+    const { data } = await axios.post("http://localhost:8080/api/v1/products", {
+      product: req.body,
+    });
     req.product = data;
     next();
   } catch (error) {
@@ -12,4 +26,49 @@ const getProduct = async (req, res, next) => {
   }
 };
 
-module.exports = { getProduct };
+const validateProductData = (req, res, next) => {
+  const { price, quantity } = req.body;
+  const errors = validateProductDataComplete(req.body);
+  if (!errors.length) {
+    if (isNaN(price)) {
+      res.status(400).send({
+        message: "The price of product should be a number.",
+      });
+    } else if (isNaN(quantity)) {
+      res.status(400).send({
+        message: "Quantity of product should be a number.",
+      });
+    } else {
+      next();
+    }
+  } else {
+    const missingParameters = errors.join(", ");
+    res.status(400).send({
+      message: `This parameters are required:  ${missingParameters}`,
+    });
+  }
+};
+
+const validateProductDataComplete = ({
+  name,
+  description,
+  price,
+  quantity,
+}) => {
+  let missingParameters = [];
+  if (!name || !/\S/.test(name)) {
+    missingParameters.push("name");
+  }
+  if (!description) {
+    missingParameters.push("description");
+  }
+  if (!price) {
+    missingParameters.push("price");
+  }
+  if (!quantity) {
+    missingParameters.push("quantity");
+  }
+  return missingParameters;
+};
+
+module.exports = { getProduct, createProduct, validateProductData };
